a59dc962852fe613b2946a602cd49534f01720f3
Thrineshreddy44 <tommy44302@gmail.com>
2024-12-17 21:07:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 /* ------------------------------------------Splash Screen pg--------------------------------------- */
 window.onload = function() {
-    setTimeout(function() {
-        document.getElementById("splash-screen").style.display = "none"; 
-        document.body.style.overflow = "auto"; 
+    
+    if (!sessionStorage.getItem('splashShown')) {
+        setTimeout(function() {
+            document.getElementById("splash-screen").style.display = "none"; 
+            document.body.style.overflow = "auto"; 
+            const nav = document.querySelector('nav');
+            if (nav) nav.style.display = "flex";
+        }, 3000);
+
+      
+        sessionStorage.setItem('splashShown', 'true');
+    } else {
+        
+        document.getElementById("splash-screen").style.display = "none";
+        document.body.style.overflow = "auto";
         const nav = document.querySelector('nav');
-        nav.style.display = "flex";
-    }, 3000); 
+        if (nav) nav.style.display = "flex";
+    }
 };
+
